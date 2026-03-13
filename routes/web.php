ed38b333dde198a3a81eaf54489fa40dd5f7b318
LuisACG7 <cruz.guerrero.luis1jvesp@gmail.com>
2026-03-13 07:28:02 -0600

    Configuración de rutas OAuth para Discord y Twitch

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('login');
 });
+
+Route::get('/login/discord', [AuthController::class, 'redirectDiscord']);
+Route::get('/callback/discord', [AuthController::class, 'handleDiscord']);
+
+Route::get('/login/twitch', [AuthController::class, 'redirectTwitch']);
+Route::get('/callback/twitch', [AuthController::class, 'handleTwitch']);
